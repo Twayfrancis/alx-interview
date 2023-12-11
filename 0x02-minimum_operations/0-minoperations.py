b16bb7f862bc17ec12299391d5d07c442114fccf
twayfrancis <tawaifrancis@gmail.com>
2023-12-11 15:33:25 +0300

    update documentation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""
-    Prototype: def minOperations(n)
-    Returns an integer
-    If n is impossible to achieve, return 0
-"""
-
-
 def minOperations(n):
     """
-        Function minOperations
-        Returns an integer
+    Calculate minimum number of operations needed to get `n` 'H' characters.
+
+    function works by creating an array `dp` where `dp[i]` is the minimum
+    number of operations to get `i` 'H' char. It initializes `dp[i]` to `i`
+    because worst case is to copy and paste 'H' `i` times. Then it finds the
+    largest factor of `i` and updates `dp[i]` to be min num of operations
+    to get `j` 'H' char plus numb of times we can paste 'H' to get `i`
+    'H' characters.
+
+    Parameters:
+    n (int): The number of 'H' characters to get.
+
+    Returns:
+    int: minimum number of operations needed to get `n` 'H' characters. If `n`
+         is impossible to achieve, return 0.
     """
     if n <= 1:
         return 0
