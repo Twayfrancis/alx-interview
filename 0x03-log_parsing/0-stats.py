04acad504bf00d6efa56a8a18d16948b5a0ef277
Twayfrancis <tawaifrancis@gmail.com>
2023-12-19 11:30:52 +0300

    cript that reads stdin line by line and computes metrics:
    Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size> (if the format is not this one, the line must be skipped)
    After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
    Total file size: File size: <total size>
    where <total size> is the sum of all previous <file size> (see input format above)
    Number of lines by status code:
    possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
    if a status code doesn’t appear or is not an integer, don’t print anything for this status code
    format: <status code>: <number>
    status codes should be printed in ascending order

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""
+script that reads stdin line by line and computes metrics
+"""
+import sys
+import signal
+
+# Initialize variables
+status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
+status_dict = dict.fromkeys(status_codes, 0)
+total_size = 0
+line_count = 0
+
+
+# Function to print statistics
+def print_stats():
+    print("File size: {}".format(total_size))
+    for code in sorted(status_dict):
+        if status_dict[code] > 0:
+            print("{}: {}".format(code, status_dict[code]))
+
+
+# Function to handle keyboard interruption
+def signal_handler(sig, frame):
+    print_stats()
+    sys.exit(0)
+
+
+# Set signal handler
+signal.signal(signal.SIGINT, signal_handler)
+
+try:
+    for line in sys.stdin:
+        try:
+            parts = line.split()
+            size = int(parts[-1])
+            status = int(parts[-2])
+            total_size += size
+            if status in status_codes:
+                status_dict[status] += 1
+        except:
+            continue
+
+        line_count += 1
+        if line_count % 10 == 0:
+            print_stats()
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    print_stats()
